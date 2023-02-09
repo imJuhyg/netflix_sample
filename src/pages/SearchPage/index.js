7d720276bee99c141d0ce797706a7804d19d32a4
imJuhyg <limjuhyg@gmail.com>
2023-02-09 19:05:41 +0900

    Add Search Debounce

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 import axios from '../../api/axios';
+import './SearchPage.css';
+import { useDebounce } from '../../hooks/useDebounce'
 
 export default function SearchPage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -15,16 +17,18 @@ export default function SearchPage() {
   }
 
   const searchTerms = useQuery().get("q");
+  const debouncedSearchTerms = useDebounce(searchTerms, 500);
+  console.log("debouncedSearchTerms:", debouncedSearchTerms);
 
   useEffect(() => {
-    if (searchTerms) {
-      fetchSearchMovie(searchTerms);
+    if (debouncedSearchTerms) {
+      fetchSearchMovie(debouncedSearchTerms);
     }
-  }, [searchTerms]);
+  }, [debouncedSearchTerms]);
 
-  const fetchSearchMovie = async (searchTerms) => {
+  const fetchSearchMovie = async (debouncedSearchTerms) => {
     try {
-      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerms}`);
+      const request = await axios.get(`/search/multi?include_adult=false&query=${debouncedSearchTerms}`);
       setSearchResults(request.data.results);
 
     } catch (error) {
@@ -32,12 +36,35 @@ export default function SearchPage() {
     }
   }
 
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
 
-  
+            return (
+              <div className="movie" key={movie.id}>
+                <div className="movie__column-poster">
+                  <img
+                    src={movieImageUrl} alt="movie image"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            )
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자하는 검색어 "{debouncedSearchTerms}"에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    )
+  }
 
-  return (
-    <div>
 
-    </div>
-  )
+  return renderSearchResults();
 }
