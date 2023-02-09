1adeb503bd8e4b676a2f309151d8a9c996c68d63
imJuhyg <limjuhyg@gmail.com>
2023-02-09 19:25:37 +0900

    Add Movie DetailPage

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -4,13 +4,11 @@ export const useDebounce = (value, delay) => {
     const [debounceValue, setDebounceValue] = useState(value);
 
     useEffect(() => {
-        console.log("hooks updated");
         const handler = setTimeout(() => { // 일정시간(delay) 뒤에 로직 실행
             setDebounceValue(value);
         }, delay);
 
         return () => {
-            console.log("hooks return");
             clearTimeout(handler); // setTimeout 취소
         };
     }, [value, delay]);
